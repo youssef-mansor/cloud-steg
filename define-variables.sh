170697cb4c3f010e6b8618d69f4c641dd089683f
youssef-mansor <youssef-mansour@aucegypt.edu>
2025-12-07 05:54:59 +0200

    deleted credentials - security issue

--- define-variables.sh
+++ define-variables.sh
@@ -1,5 +1,5 @@
 export SHARED_DRIVE_ID=0AEwep46IAWKDUk9PVA
 export REGISTERED_USERS_FOLDER_ID=1P5o3QM-PicdKdYNQM9YRqaNje4fv6QNk
 export FIREBASE_BUCKET="cloud-steg-a463f.firebasestorage.app"  # Replace with YOUR bucket
-export GOOGLE_APPLICATION_CREDENTIALS="credentials/cloud-steg-a463f-firebase-adminsdk-fbsvc-484ddc44b2.json"
+export GOOGLE_APPLICATION_CREDENTIALS="credentials/cloud-steg-a463f-c92dca9b1e7b.json"
 export API_PORT=3000
